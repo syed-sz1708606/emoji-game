accfd15373bccd9261b26cf29ee3ab7ba1cc30ca
Syed Zubair <szbaz@outlook.com>
2022-11-06 10:18:12 +0300

    add bg color when emojis matched

--- component/Tile.js
+++ component/Tile.js
@@ -4,10 +4,11 @@ import React from 'react'
 const Tile = ({ emoji, index, isMatched, handleClick, isSelected, rotateBy }) => {
 
   const styles = { backgroundColor: isSelected ? "#59E391" : "transparent" }
+  const selected = { backgroundColor: isMatched ? "rgba(216, 216, 216, 0.35)" : "transparent" }
 
   return (
     <div style={styles}>
-      <Button variant="outlined" disabled={isMatched} onClick={() => handleClick(emoji, index)}>
+      <Button variant="outlined" disabled={isMatched} sx={selected} onClick={() => handleClick(emoji, index)}>
         <Typography sx={{ fontSize: 36, transform: rotateBy }} >{emoji}</Typography>
       </Button>
     </div>
